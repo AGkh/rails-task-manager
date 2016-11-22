3ad72e0d87864f77b006fb44ef385f920d4f200a
Arthur Garaud <arthurgaraud@gmail.com>
2016-11-22 14:51:32 +0100

    exercice 1 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :tasks
+#   # lire toutes les tasks
+# get "tasks" => "tasks#index"
+
+#   # lire une task
+# get "tasks/:id" => "tasks#show", as: :task
+
+#   # créer une task
+# get "tasks/new" => "tasks#new", as: :tasks_new
+# post "tasks" => "tasks#create"
+
+#   # update une task
+# get "tasks/:id/edit" => "tasks#edit"
+# patch "tasks/:id" => "tasks#update"
+
+#   # supprimer une task
+# delete "tasks/:id" => "tasks#destroy"
+
 end
+
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "dancing", done: false)
+Task.create(name: "football", done: false)
+Task.create(name: "washing", done: false)
